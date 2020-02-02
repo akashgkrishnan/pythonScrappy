1aa5ac866fbbf06548ca4c2dd691f6dc1dbe0c35
Akash G Krishnan <krishnanag1996@gmail.com>
2020-02-02 23:10:59 +0530

    The following has been done today 1) Created a soup constructor 2) scraped quotes and author names 3) created an automation script which will scrape from each page and check if a next page is present if present will scrape from the next page and so on till no next element is found 4) stored the quotes and author of the quotes in dictionary and appended to a blank list

---
+++ scraps.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+
+all_quotes = []
+base_url = 'http://quotes.toscrape.com/'
+url = "/page/1"
+while url:
+    res = requests.get(base_url+url)
+    print(f"now scraping {base_url}{url}")
+    soup = BeautifulSoup(res.text,"html.parser")
+    quotes = soup.find_all(class_ = "quote")
+    i = 0
+    for quote in quotes:
+        all_quotes.append({
+        'text':  quote.find(class_ = "text").get_text(),
+        "author":quote.find(class_ = "author").get_text()
+        })
+        i += 1
+
+    sleep(2)
+
+
+    next_button = soup.find(class_ = "next")
+    url = next_button.find("a")["href"] if next_button else None
+print(len(all_quotes))
